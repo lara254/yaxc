1e11a4d561e4bf376b4f5647bc6bcbebf71d43cf
Job Hernandez Lara <lara2993@proton.me>
2025-01-20 22:14:54 -0800

    remove main function

--- compiler.go
+++ compiler.go
@@ -323,16 +323,3 @@ func PrintSelect(ins Instructions) {
 	fmt.Println(ins.Instructs)
 }
 
-
-func main() {
-	input := "(let ((i 0)) (if (< i 3) 2 3))"
-	//input := "(let ((i (let ((d 4)) (+ 3 3)))) (if (< 2 3) 2 3))"
-	ast, _ := Parse(input)
-	mon := ToAnf(ast, 0)
-	mon_ := PrintMon(mon)
-	fmt.Println(mon_)
-	ss := SelectInstructions(mon)
-	PrintSelect(ss)
-	
-}
-
